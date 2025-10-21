e8c6543256d8f3570b4823dc5c3deece0ae9f67b
YamasakiHiroto <h1202516004@hiratagakuen.onmicrosoft.com>
2025-10-21 11:42:40 +0900

    changescene や　ソースツリーの問題修正

--- Team_B/Assets/Scenes/Changescene.cs
+++ Team_B/Assets/Scenes/Changescene.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene1 : MonoBehaviour
+public class Changescene : MonoBehaviour
 {
     public string sceneName;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     //ƒV[ƒ“‚ğ“Ç‚İ‚Ş
