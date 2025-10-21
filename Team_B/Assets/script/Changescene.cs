2e311f58876e68db143d2925a0ec47423c0b59a7
Nakamura shunya <h1202519007@hiratagakuen.onmicrosoft.com>
2025-10-21 12:06:57 +0900

    aa

--- Team_B/Assets/script/Changescene.cs
+++ Team_B/Assets/script/Changescene.cs
@@ -1,16 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Changescene : MonoBehaviour
 {
+    public string sceneName;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    //ÉVÅ[ÉìÇì«Ç›çûÇﬁ
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
